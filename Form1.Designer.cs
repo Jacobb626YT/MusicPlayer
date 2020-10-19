c34f31e181b09acafa1b6a6ef6247b71ffb179f7
Jacobb626YT <66649071+Jacobb626YT@users.noreply.github.com>
2020-10-19 01:58:19 -0400

    Soucre Code You Happy

---
+++ Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Music
+{
+    partial class Music
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Music));
+            this.Stop = new MaterialSkin.Controls.MaterialButton();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.Play = new MaterialSkin.Controls.MaterialButton();
+            this.SuspendLayout();
+            // 
+            // Stop
+            // 
+            resources.ApplyResources(this.Stop, "Stop");
+            this.Stop.Depth = 0;
+            this.Stop.DrawShadows = true;
+            this.Stop.HighEmphasis = true;
+            this.Stop.Icon = null;
+            this.Stop.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Stop.Name = "Stop";
+            this.Stop.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.Stop.UseAccentColor = false;
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // materialLabel1
+            // 
+            resources.ApplyResources(this.materialLabel1, "materialLabel1");
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            // 
+            // Play
+            // 
+            resources.ApplyResources(this.Play, "Play");
+            this.Play.Depth = 0;
+            this.Play.DrawShadows = true;
+            this.Play.HighEmphasis = true;
+            this.Play.Icon = null;
+            this.Play.MouseState = MaterialSkin.MouseState.HOVER;
+            this.Play.Name = "Play";
+            this.Play.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.Play.UseAccentColor = false;
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Music
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Play);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.Stop);
+            this.Name = "Music";
+            this.Sizable = false;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialButton Stop;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialButton Play;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+// Import the Material Skin
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace Music
+{
+    public partial class Music : MaterialForm
+    {
+        public Music()
+        {
+            InitializeComponent();
+
+            // Create a material theme manager and add the form to manage (this)
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+
+            // Configure color schema
+            materialSkinManager.ColorScheme = new ColorScheme(
+                Primary.Blue400, Primary.Blue500,
+                Primary.Blue500, Accent.LightBlue200,
+                TextShade.WHITE
+            );
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Stop.Visible = false;
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = @".\Song.wav";
+            sp.PlayLooping();
+            Stop.Visible = true;
+            Play.Visible = false;
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            SoundPlayer sp = new SoundPlayer();
+            sp.SoundLocation = @".\Song.wav";
+            sp.Stop();
+            Play.Visible = true;
+            Stop.Visible = false;
+        }
+    }
+}
